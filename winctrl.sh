2c80f445912d4e6dda5a723b304be399f8ee0e75
Vathanak Mao <vathanakmao@gmail.com>
2023-02-04 01:10:34 +0700

    Worked with any windows minimized by the script or manually done by a user

--- winctrl.sh
+++ winctrl.sh
@@ -1,67 +1,49 @@
 #!/bin/bash
 
-ids_filename="/run/lock/winctrl.ids"
-tmp_ids_filename="/run/lock/winctrl.ids.tmp"
+# Redirect the output of echo command to a log file
+exec > >(tee -i /run/lock/winctrl.log)
+exec 2>&1
+
 
 if [[ $1 = "-v" ]]; then
 	if [[ $2 = false ]]; then
-		echo ">> Start minimizing the app window."
+		echo ">> Checking if there is an active window..."
 
 		active_winid=$(printf 0x%x $(xdotool getactivewindow)) # convert from dec to hex
-		echo ">> active_winid=$active_winid"
+		echo ">> Found an active/opening window <$active_winid>"
 		
 		if [[ $active_winid -gt 0x0 ]]; then
-			xdotool windowminimize $active_winid	# minimize the window
-			
-			# remove previously saved window id
-			# this might happen when the user manually open the window and use this script to hide it again
-			sed -i "/$active_winid/d" $ids_filename
-
-			# append a new window id
-			echo $active_winid >> $ids_filename	# append the window id in cache
-		fi 	
+			xdotool windowminimize $active_winid
+			echo ">> Minimized the window <$active_winid>"
+		else
+			echo ">> No active window found!"
+		fi
 	else 
-		echo ">> Start raising the app window."
+		echo ">> Checking if there is a minimized window..."
 		
-		while true
-		do
-			# If the cache file exists and not empty 
-			if [[ -f "${ids_filename}" && -s "${ids_filename}" ]]; then
-			
-				# get the last minimized window id
-				lastminimized_winid=$(tail -n 1 $ids_filename)
-				echo ">> lastminimized_winid=$lastminimized_winid"
+		str_ids=$(xprop -root | grep "^_NET_CLIENT_LIST_STACKING" | cut -d'#' -f2)
+		IFS=', ' read -r -a ids <<< "$str_ids"
+		echo "${ids[@]}"
+
+		for (( idx=${#ids[@]}-1 ; idx>=0 ; idx-- )) ; do
+			echo ">> Looping: ids[$idx]=$((ids[idx]))"
+
+			# If the window is hidden/minimized
+			if xwininfo -all -id $((ids[idx])) |grep "Hidden"; then
+				echo ">> Found a minimized window <$((ids[idx]))>"
 				
-				# remove the last minimized window id from the cache
-				head -n -1 $ids_filename > $tmp_ids_filename
-				mv $tmp_ids_filename $ids_filename
+				wmctrl -ia $((ids[idx]))
+				echo ">> Unminimized the window <$((ids[idx]))>"
 				
-				# if it's being minimized (the user hasn't manually opened it)
-				if xwininfo -all -id $lastminimized_winid | grep "Hidden"; then
-	  				# activate the window
-					xdotool windowactivate $lastminimized_winid
-					
-					echo ">> Raised the app window <$lastminimized_winid>."
-					break
-	  			else 
-	  				echo ">> The app window <$lastminimized_winid> has manually opened by the user (not using this script)."
-	  				continue
-	  			fi
-				
-			else # the cache file does not exist or empty (no minimized window id saved)
-				echo ">> The cache file $ids_filename does not exist or is empty so can't raise the app window <$lastminimized_winid>."
-			
-				# If there is minimized app, which is manually done by a user, then????
-				# else
-				break
+				exit 0
 			fi
 		done
+		
+		echo ">> No minimized window found!"
 	fi
 else
 	echo "Please specify an option, for example 'appctrl -v true', to raise and focus on the app window, and 'appctrl -v false' to minimize the app window."
-	
 fi
-
 
 
 
